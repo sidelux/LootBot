9a61d2d62ef542b146131cfa5ce405598f4c9d34
Edoardo Cortese <redfenix45@gmail.com>
2018-10-04 17:07:16 +0200

    Fixed config for maintenance bots

--- lbp_problem.js
+++ lbp_problem.js
@@ -3,14 +3,10 @@ process.on('uncaughtException', function (err) {
 });
 
 var TelegramBot = require('node-telegram-bot-api');
-var token = '236880746:AAEolJ-Dpe_gQdnGxksGFTb1ubMj03PVhw4';
+var config = require('./config.js');
+var token = config.plustoken;
 var bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', function (message) {
         console.log("Manutenzione: " + message.from.username + ": " + message.text);
-
-        var text = "Manutenzione, riprova tra poco";
-        //text = "Problemi ai server, ci scusiamo per il disagio. Riprova più tardi.\nIntanto accedi al gruppo pubblico per verificare lo stato del gioco: https://telegram.me/joinchat/AThc-z_EfojvcE8mbGw$
-
-        //bot.sendMessage(message.chat.id, text);
 });
