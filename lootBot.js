19c4000a5b44b5556be5d79fac19c6eb10f9a276
sidelux <redfenix45@gmail.com>
2022-06-08 16:58:05 +0200

    Added charge when map inactivity

--- lootBot.js
+++ lootBot.js
@@ -60769,7 +60769,8 @@ async function setBattleTimeElapsed(element, index, array) {
 					}
 
 					bot.sendMessage(chat_id, "Troppi turni sono scaduti!\nDal nulla arriva una freccia a gran velocità e decreta il tuo avversario come vincitore dello scontro!");
-					bot.sendMessage(enemy_chat_id, "L'avversario ha perso troppi turni!\nHai vinto lo scontro!" + enemy_text, html);
+					await addDungeonEnergy(enemy_id, 1);
+					bot.sendMessage(enemy_chat_id, "L'avversario ha perso troppi turni!\nHai vinto lo scontro e ricevuto 1 Carica Esplorativa aggiuntiva!" + enemy_text, html);
 
 					connection.query('UPDATE map_lobby SET my_turn = 0, battle_timeout = NULL, battle_time_elapsed = battle_time_elapsed + ' + diff + query + ' WHERE player_id = ' + player_id, function (err, rows, fields) {
 						if (err) throw err;
